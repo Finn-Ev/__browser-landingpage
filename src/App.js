97b21bcb925b096b97f30213bc063cde41da0ce9
Finn Evermann <finnevermann@gmx.de>
2020-04-07 00:36:29 +0200

    disabled mobile support

--- src/App.js
+++ src/App.js
@@ -9,31 +9,34 @@ const App = () => {
   const [background, setBackground] = useState();
 
   useEffect(() => {
-  
-     if(localStorage.getItem("background")){
+
+    if (localStorage.getItem("background")) {
       setBackground(localStorage.getItem("background"))
-     } 
- 
+    }
+
   }, [background])
 
   const changeBackground = (background, maintainBackground) => {
-    if(maintainBackground){
+    if (maintainBackground) {
       localStorage.setItem("background", background)
       setBackground(localStorage.getItem("background"));
     }
-    else if(!maintainBackground ) {
+    else if (!maintainBackground) {
       setBackground(background);
-    } 
+    }
   };
 
   return (
-    <div className={`${background}`}>
-      <div id="App">
-        <Header />
-        <Clock changeBackground={changeBackground} />
-        <Footer changeBackground={changeBackground} />
+    <>
+      <div className={`${background}`}>
+        <div id="App">
+          <Header />
+          <Clock changeBackground={changeBackground} />
+          <Footer changeBackground={changeBackground} />
+        </div>
       </div>
-    </div>
+      <div className="mobile-disclaimer"><p>Diese Website ist nicht für mobile Endgeräte verfügbar</p></div>
+    </>
 
   );
 };
