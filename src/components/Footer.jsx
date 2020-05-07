2608780ff7cafb08ac6d5ea9357b00d8751ac098
Finn Evermann <finnevermann@gmx.de>
2020-05-07 15:39:37 +0200

    changed footer URL

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import Links from "./FooterComponents/Links"
-import Dropdown from "./FooterComponents/DropdownComp"
-import Searchbar from './FooterComponents/Searchbar'
+import React from "react";
+import Links from "./FooterComponents/Links";
+import Dropdown from "./FooterComponents/DropdownComp";
+import Searchbar from "./FooterComponents/Searchbar";
 
 function Footer(props) {
-    return (
-        <div className="footer">
-            <Searchbar/>
-            <Dropdown changeBackground={props.changeBackground} />
-            <Links />
-            <span id="copyright"><a href="https://finnevermann.netlify.com/">von&nbsp;Finn Evermann</a></span>
-        </div>
-
-    )
+  return (
+    <div className="footer">
+      <Searchbar />
+      <Dropdown changeBackground={props.changeBackground} />
+      <Links />
+      <span id="copyright">
+        <a href="https://finnevermann.de/">von&nbsp;Finn Evermann</a>
+      </span>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/FooterComponents/Links.jsx
+++ src/components/FooterComponents/Links.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Links() {
-    return (
-        <>
-        <div className="links">
-                <a href="https://www.google.de/"><i id="google" className="fab fa-google"></i></a>
-                <a href="https://www.youtube.com/?gl=DE&hl=de"><i id="youtube" className="fab fa-youtube"></i></a>
-                <a href="https://www.instagram.com/?hl=de"><i id="instagram" className="fab fa-instagram"></i></a>
-                <a href="https://de-de.facebook.com/"><i id="facebook" className="fab fa-facebook"></i></a>
-                <a href="https://twitter.com/?lang=de"><i id="twitter" className="fab fa-twitter"></i></a>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="links">
+        <a href="https://www.google.de/">
+          <i id="google" className="fab fa-google"></i>
+        </a>
+        <a href="https://www.youtube.com/?gl=DE&hl=de">
+          <i id="youtube" className="fab fa-youtube"></i>
+        </a>
+        <a href="https://www.instagram.com/?hl=de">
+          <i id="instagram" className="fab fa-instagram"></i>
+        </a>
+        <a href="https://de-de.facebook.com/">
+          <i id="facebook" className="fab fa-facebook"></i>
+        </a>
+        <a href="https://twitter.com/?lang=de">
+          <i id="twitter" className="fab fa-twitter"></i>
+        </a>
+      </div>
+    </>
+  );
 }
 
-export default Links
+export default Links;
