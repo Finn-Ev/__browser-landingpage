adf8541ccbb377fcb62bb0bc691d900610feeeac
Finn Evermann <finnevermann@gmx.de>
2020-05-18 06:30:19 +0200

    adapted morning time

--- src/components/clock/Clock.jsx
+++ src/components/clock/Clock.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import "./clock.scss"
+import "./clock.scss";
 
 class Clock extends Component {
-  constructor({props}) {
+  constructor({ props }) {
     super(props);
     this.state = {
       time: new Date(),
@@ -15,102 +15,98 @@ class Clock extends Component {
 
       usernameSet: localStorage.getItem("username") ? true : false,
 
-      hovering: false
+      hovering: false,
     };
   }
 
   tick = () => {
     setInterval(() => {
       this.setState({
-        time: new Date()
+        time: new Date(),
       });
 
       // Add "0" when needed
-      
+
       // Add "0" (hours)
       if (this.state.time.getHours() < 10) {
         this.setState({
-          hours: `0${this.state.time.getHours()}`
+          hours: `0${this.state.time.getHours()}`,
         });
       } else {
         this.setState({
-          hours: this.state.time.getHours()
+          hours: this.state.time.getHours(),
         });
       }
 
       // Add "0" (minutes)
       if (this.state.time.getMinutes() < 10) {
         this.setState({
-          minutes: `0${this.state.time.getMinutes()}`
+          minutes: `0${this.state.time.getMinutes()}`,
         });
       } else {
         this.setState({
-          minutes: this.state.time.getMinutes()
+          minutes: this.state.time.getMinutes(),
         });
       }
 
       // Add "0" (seconds)
       if (this.state.time.getSeconds() < 10) {
         this.setState({
-          seconds: `0${this.state.time.getSeconds()}`
+          seconds: `0${this.state.time.getSeconds()}`,
         });
       } else {
         this.setState({
-          seconds: this.state.time.getSeconds()
+          seconds: this.state.time.getSeconds(),
         });
       }
     }, 500);
   };
 
   getTageszeit() {
-    const { changeBackground } = this.props
-      if (this.state.hours > 6 && this.state.hours < 12) {
-        this.setState({
-          msg: "Guten Morgen,"
-        });
-        changeBackground("firewatch-forest-gold-morning", false)
-
-      } else if (this.state.hours >= 12 && this.state.hours < 18) {
-        this.setState({
-          msg: "Guten Tag,"
-        });
-        changeBackground("firewatch-forest-day-green", false)
-
-      } else if (this.state.hours >= 18 && this.state.hours < 24) {
-        this.setState({
-          msg: "Guten Abend,"
-        });
-        changeBackground("firewatch-forest-evening-red", false)
-
-      } else {
-        this.setState({
-          msg: "Gute Nacht,"
-        });
-        changeBackground("firewatch-forest-night-green", false)
-      }
-    ;
+    const { changeBackground } = this.props;
+    if (this.state.hours > 4 && this.state.hours < 12) {
+      this.setState({
+        msg: "Guten Morgen,",
+      });
+      changeBackground("firewatch-forest-gold-morning", false);
+    } else if (this.state.hours >= 12 && this.state.hours < 18) {
+      this.setState({
+        msg: "Guten Tag,",
+      });
+      changeBackground("firewatch-forest-day-green", false);
+    } else if (this.state.hours >= 18 && this.state.hours < 24) {
+      this.setState({
+        msg: "Guten Abend,",
+      });
+      changeBackground("firewatch-forest-evening-red", false);
+    } else {
+      this.setState({
+        msg: "Gute Nacht,",
+      });
+      changeBackground("firewatch-forest-night-green", false);
+    }
   }
 
   handleSubmit = () => {
     this.setState({ usernameSet: true });
   };
 
-  handleChange = e => {
+  handleChange = (e) => {
     localStorage.setItem("username", e.target.value);
     this.setState({
-      username: localStorage.getItem("username")
+      username: localStorage.getItem("username"),
     });
   };
 
   reset = () => {
     this.setState({
-      usernameSet: localStorage.removeItem("username")
+      usernameSet: localStorage.removeItem("username"),
     });
   };
 
   componentDidMount() {
     this.tick();
-    this.getTageszeit()
+    this.getTageszeit();
   }
 
   render() {
