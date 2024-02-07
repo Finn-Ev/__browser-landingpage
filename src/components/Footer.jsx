b9a69ead0bb431ff5812cc2a2e9e1a731db3d9ee
Finn-Ev <finn.evermann@icloud.com>
2024-02-07 21:20:57 +0100

    remove warnings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Links from "./FooterComponents/Links";
-import Searchbar from "./FooterComponents/Searchbar";
+
 
 function Footer(props) {
   return (
     <div className="footer">
-      {/* <Searchbar /> */}
-      {/* <Links /> */}
+
     </div>
   );
 }
